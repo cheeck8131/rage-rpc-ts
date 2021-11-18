b1d8fbe3825863cf68622d4b249401e7c4afaab8
cheeck <vova.rupasov8131@gmail.com>
2021-11-19 01:10:23 +0300

    readme & delete 'as' from tools

--- src/common/tools.ts
+++ src/common/tools.ts
@@ -12,11 +12,11 @@ declare global {
   }
 }
 
-(String.prototype as any).capitalize = function capitalize() {
+(<any>String.prototype).capitalize = function capitalize() {
   return this.charAt(0).toUpperCase() + this.slice(1);
 };
 
-(String.prototype as any).replaceAll = function (
+(<any>String.prototype).replaceAll = function (
   this: string,
   str: string,
   newStr: string
@@ -28,7 +28,7 @@ declare global {
   return this.replace(new RegExp(str, "g"), newStr);
 };
 
-(Array.prototype as any).remove = function (value: any) {
+(<any>Array.prototype).remove = function (value: any) {
   const index = this.indexOf(value);
   if (index !== -1) {
     this.splice(index, 1);
