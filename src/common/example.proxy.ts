72a4a90ce7396adb47fc9adc7923b8932500fc39
cheeck <vova.rupasov8131@gmail.com>
2021-11-19 00:57:25 +0300

    examples && tools added

---
+++ src/common/example.proxy.ts
@@ -0,0 +1,68 @@
+import rpc from "rage-rpc";
+import { RuntimeTypes } from "./types";
+
+const serverProxyCache = new Map<string, InstanceType<typeof Proxy>>();
+export const server = new Proxy(
+  {},
+  {
+    get(_, service: string) {
+      if (serverProxyCache.has(service)) {
+        return serverProxyCache.get(service);
+      } else {
+        const proxy = new Proxy(
+          {},
+          {
+            get(_, event: string) {
+              const call = (noRet: boolean, ...args: any[]) => {
+                const eventName = `${service.capitalize()}${event}`;
+                return rpc.callServer(eventName, args, { timeout: 60 * 1000, noRet });
+              };
+
+              call.bind(null, true);
+              (call as any).noRet = (...args: any[]) => {
+                call(false, ...args);
+              };
+
+              return call;
+            },
+          }
+        );
+        serverProxyCache.set(service, proxy);
+        return proxy;
+      }
+    },
+  }
+) as RuntimeTypes.IProxy<ServiceGenericType>;
+
+const clientProxyCache = new Map<string, InstanceType<typeof Proxy>>();
+export const client = new Proxy(
+  {},
+  {
+    get(_, service: string) {
+      if (clientProxyCache.has(service)) {
+        return clientProxyCache.get(service);
+      } else {
+        const proxy = new Proxy(
+          {},
+          {
+            get(_, event: string) {
+              const call = (noRet: boolean, ...args: any[]) => {
+                const eventName = `${service.capitalize()}${event}`;
+                return rpc.callServer(eventName, args, { timeout: 60 * 1000, noRet });
+              };
+
+              call.bind(null, true);
+              (call as any).noRet = (...args: any[]) => {
+                call(false, ...args);
+              };
+
+              return call;
+            },
+          }
+        );
+        clientProxyCache.set(service, proxy);
+        return proxy;
+      }
+    },
+  }
+) as RuntimeTypes.IProxy<any>; // any to client type
